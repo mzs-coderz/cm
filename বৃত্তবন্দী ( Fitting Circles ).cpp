4696729ec995e69f96184890b6e24c66f143d847
Safayet Islam Anonn'o <anonno.anonno@gmail.com>
2015-06-17 00:03:23 +0600

    Create বৃত্তবন্দী ( Fitting Circles ).cpp

---
+++ বৃত্তবন্দী ( Fitting Circles ).cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define pi 2*acos(0)
+double R,c,theta,angle1,angle2;
+
+int main()
+{
+    int T, n;
+    scanf("%d",&T);
+    for(int i=1; i<=T; i++)
+    {
+        scanf("%lf %d",&R, &n);
+        if(n==2)
+            printf("%.2lf\n",(2*R)/4);
+        else
+        {
+            angle1 = (2*pi)/n;
+            angle2 = (pi-angle1)/2;
+            c = sin(angle1)/sin(angle2);
+            printf("%.2lf\n",(c*R)/(2+c));
+        }
+    }
+    return 0;
+}
